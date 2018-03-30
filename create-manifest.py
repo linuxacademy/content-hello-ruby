17db31272e3ed46a0e48f6dca93c02d22581f683
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-03-29 22:33:36 -0500

    add blue green

---
+++ create-manifest.py
@@ -0,0 +1,25 @@
+import string
+import random
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+random_string = id_generator(16)
+
+file = open("manifest.yml","w")
+file.write("applications:\n")
+file.write("- name: new-site\n")
+file.write("  memory: 256m\n")
+file.write("  disk_quota: 256m\n")
+file.write("  path: ./new/\n")
+file.write("  routes:\n")
+file.write("    - route: blue-green-" + random_string + ".cfapps.io\n")
+
+
+file.write("- name: old-site\n")
+file.write("  memory: 256m\n")
+file.write("  disk_quota: 256m\n")
+file.write("  path: ./old/\n")
+file.write("  routes:\n")
+file.write("    - route: blue-green-" + random_string + ".cfapps.io\n")
+file.close()
---
+++ old/app.rb
@@ -0,0 +1,5 @@
+require 'sinatra'
+
+get '/' do
+  "Old version"
+end
